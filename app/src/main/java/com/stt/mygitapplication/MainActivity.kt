682ce18ca18a5fc944b98c9377a1c0270942797c
hgb <243775640@qq.com>
2019-01-07 16:48:19 +0800

    分支修改

--- app/src/main/java/com/stt/mygitapplication/MainActivity.kt
+++ app/src/main/java/com/stt/mygitapplication/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var ss = "fafas"
+        //在分支上修改了内容
     }
 }
